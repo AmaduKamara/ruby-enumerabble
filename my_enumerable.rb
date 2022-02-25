b31d72a4a3a9e739b3ec649b91a6aa8e3413dc54
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-02-25 20:50:41 +0100

    Add any? and filter methods & Test them

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -9,4 +9,22 @@ module MyEnumerable
     true
   end
 
+  def any?(&block)
+    each do |item|
+      if block.call(item)
+        return true
+      end
+    end
+    false
+  end
+
+  def filter(&block)
+    result = []
+    each do |item|
+      if block.call(item)
+        result << item
+      end
+    end
+    result
+  end
 end
